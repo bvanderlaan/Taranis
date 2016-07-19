5e9e434b01b3ccecb5d56a954e1dd51d8ef2e334
bvanderlaan <brad.vanderlaan@gmail.com>
2016-07-18 22:54:49 -0400

    Add script to publish doxygen documents to the gh-pages repository on GitHub.

---
+++ config/publish.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+REPO_PATH=git@github.com:bvanderlaan/taranis.git
+DOCS_PATH=gh-pages/docs
+HTML_PATH=build/doc/html
+COMMIT_USER="Documentation Builder"
+COMMIT_EMAIL="brad.vanderlaan@gmail.com"
+
+# Exit with nonzero exit code if anything fails
+set -e
+
+@echo 'Uploading documentation to the gh-pages branch...'
+
+# Checkout and clean out the docs folder
+git clone -b gh-pages "${REPO_PATH}" --single-branch
+cd "${DOCS_PATH}"
+git rm -rf .
+cd -
+
+if [ -d "${HTML_PATH}"] && [ -f "${HTML_PATH}/index.html" ]; then
+    # copy the newly created documents into the gh-pages docs folder.
+    cp -R "${HTML_PATH}" "${DOCS_PATH}"
+
+    # commit the new documentation
+    cd ${DOCS_PATH}
+    git add --all
+    git config user.name "${COMMIT_USER}"
+    git config user.email "${COMMIT_EMAIL}"
+    git commit -m"Automated documentation build for changeset ${TRAVIS_COMMIT}"
+    git push origin gh-pages > /dev/null 2>&1
+    cd -
+else
+    @echo '' >&2
+    @echo 'Warning: No documentation (html) files have been found!' >&2
+    @echo 'Warning: Not going to push the documentation to GitHub!' >&2
+    exit 1
+fi
