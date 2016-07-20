dcc92aa9d5ab99549bc6255d2ad975910624dd62
bvanderlaan <brad.vanderlaan@gmail.com>
2016-07-19 23:25:07 -0400

    Add debug message.

--- config/publish.sh
+++ config/publish.sh
@@ -8,10 +8,12 @@ COMMIT_EMAIL="brad.vanderlaan@gmail.com"
 # Only update docs on master branch.
 if [ ${TRAVIS_BRANCH} != "master" ]; then exit 0; fi
 
+echo ${TRAVIS_OS_NAME}
+
 # The version of OSX (10.9) used by default on Travis-ci has an older version of bash (3.2.1)
 # which uses a different expr syntax. to deal with this issue quickly and since the first build
 # is a linux machine simply skip publication of the doxygen documentation on osx machines.
-if [ "${TRAVIS_OS_NAME}" != "linux" ]; then exit 0; fi
+if [ ${TRAVIS_OS_NAME} != "linux" ]; then exit 0; fi
 
 # Only update docs for the first or only build job. This way we are not updating the docs 
 # multiple times on a matrix configuration.
