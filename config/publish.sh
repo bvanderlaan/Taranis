0bd2df469e58d0d149bf444f23e6662f0b4f3937
bvanderlaan <brad.vanderlaan@gmail.com>
2016-07-19 22:14:45 -0400

    Add checks so that the doxygen publish script is only executed once. On matrix builds which run the same commit against multiple configurations, i.e. osx and linux, we don't want to update the documents multiple times nor do we want to update the documentation for branches; only master.
    This change set adds checks to make sure the script only publishes the
    documentation if its run against the master branch and is the first or only
    build job.

--- config/publish.sh
+++ config/publish.sh
@@ -5,6 +5,14 @@ HTML_PATH=build/doc/html
 COMMIT_USER="Documentation Builder"
 COMMIT_EMAIL="brad.vanderlaan@gmail.com"
 
+# Only update docs on master branch.
+if [ ${TRAVIS_BRANCH} != "master" ]; then exit 0; fi
+
+# Only update docs for the first or only build job. This way we are not updating the docs 
+# multiple times on a matrix configuration.
+BUILD_JOB_SEP_POS=$(expr index ${TRAVIS_JOB_NUMBER} .)
+if [ ${BUILD_JOB_SEP_POS} > 0] && [ ${TRAVIS_JOB_NUMBER:${BUILD_JOB_SEP_POS}} > 1 ]; then exit 0; fi
+
 # Exit with nonzero exit code if anything fails
 set -e
 
