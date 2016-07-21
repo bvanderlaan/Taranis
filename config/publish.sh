81af0485901650b04b803f7b059972057d575177
bvanderlaan <brad.vanderlaan@gmail.com>
2016-07-20 20:15:37 -0400

    Fix only publish on first job restriction. I think I've got this bash script figured out, I don't use bash often if you couldn't tell. This should now do a numerical comparision of the current build number to figure out if its (a) a multi configuration build or not and if it is then (b) tell if its the first multi configuration job.
    Only then will it publish an update to the doxygen documentation.

--- config/publish.sh
+++ config/publish.sh
@@ -8,8 +8,6 @@ COMMIT_EMAIL="brad.vanderlaan@gmail.com"
 # Only update docs on master branch.
 if [ ${TRAVIS_BRANCH} != "master" ]; then exit 0; fi
 
-echo ${TRAVIS_OS_NAME}
-
 # The version of OSX (10.9) used by default on Travis-ci has an older version of bash (3.2.1)
 # which uses a different expr syntax. to deal with this issue quickly and since the first build
 # is a linux machine simply skip publication of the doxygen documentation on osx machines.
@@ -18,11 +16,7 @@ if [ ${TRAVIS_OS_NAME} != "linux" ]; then exit 0; fi
 # Only update docs for the first or only build job. This way we are not updating the docs 
 # multiple times on a matrix configuration.
 BUILD_JOB_SEP_POS=$(expr index ${TRAVIS_JOB_NUMBER} .)
-
-echo ${BUILD_JOB_SEP_POS}
-echo ${TRAVIS_JOB_NUMBER#*.}
-
-if [ ${BUILD_JOB_SEP_POS} > 0 ] && [ ${TRAVIS_JOB_NUMBER#*.} > 1 ]; then exit 0; fi
+if [ ${BUILD_JOB_SEP_POS} -gt 0 ] && [ ${TRAVIS_JOB_NUMBER#*.} -gt 1 ]; then exit 0; fi
 
 # Exit with nonzero exit code if anything fails
 set -e
