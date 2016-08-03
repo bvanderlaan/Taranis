812d77c453b8920eaf102cea65f7c5b4e71b10c6
Documentation Builder <brad.vanderlaan@gmail.com>
2016-08-03 02:35:17 +0000

    Automated documentation build for changeset 017a3fcfa2e5b5e675819cc16282f0012d461c3c

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['versionargumentredefinitionexception',['VersionArgumentRedefinitionException',['../class_taranis_1_1_version_argument_redefinition_exception.html',1,'Taranis']]],
   ['versionshortnamecollisionexception',['VersionShortNameCollisionException',['../class_taranis_1_1_version_short_name_collision_exception.html',1,'Taranis']]]
 ];
